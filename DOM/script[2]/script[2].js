0f77d08164874f174778b8958171ba018714d03a
Ratnesh125 <87529730+Ratnesh125@users.noreply.github.com>
2023-03-18 12:34:46 +0530

    Create script[2].js

---
+++ DOM/script[2]/script[2].js
@@ -0,0 +1,7 @@
+document.querySelectorAll("ul li a")[0].style.color="red";
+document.querySelectorAll("button")[0].innerHTML="click here";
+document.querySelectorAll("button")[0].style.backgroundColor="blue";
+document.querySelectorAll("ul li")[2].style.color="blue";
+document.querySelectorAll("h1")[0].innerHTML="welcome";
+document.querySelectorAll("h1")[0].style.textDecoration = "underline overline";
+document.querySelectorAll("input")[0].click();
